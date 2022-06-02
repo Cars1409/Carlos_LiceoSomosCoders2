dcdd782e1cb513cc89cded86a57d02d3ef99e500
Cars1409 <101503416+Cars1409@users.noreply.github.com>
2022-06-02 12:29:39 +0200

    Subiendo
    Aquí estoy subiendo nuevas pruebas para el modelo de la portada

--- Ecommerce/js/script.js
+++ Ecommerce/js/script.js
@@ -1,13 +1,13 @@
-var c1 =new Object();
-c1.fecha ="31/01/45"
-c1.lugar = "logroño"
-c1.cantante = "lulu"
-c1.plazas = 305
-c1.p_disponibles = 3
 
-var c2 =new Object();
-c2.fecha ="31/01/415"
-c2.lugar = "logroño"
-c2.cantante = "pablo alboran"
-c2.plazas = 305
-c2.p_disponibles = 3
+
+let n=0;
+while (n<=1){
+    console.log("*");
+    n++;
+
+    let n=1;
+    while (n<=2){
+        console.log("*");
+        n++;
+    }
+}
