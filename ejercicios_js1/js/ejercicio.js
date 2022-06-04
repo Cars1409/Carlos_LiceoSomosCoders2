d46cbb9d25c96f1dd457498fbdf4abf25b4830b6
Cars1409 <101503416+Cars1409@users.noreply.github.com>
2022-06-04 12:17:13 +0200

    Ejercicio de mensajes
    Aquí se muestra los mensajes que aparecen en la pantalla utilizando document.write, console.log y alert. He comentado mis impresiones sobre su utilidad.

--- ejercicios_js1/js/ejercicio.js
+++ ejercicios_js1/js/ejercicio.js
@@ -1,1 +1,13 @@
-console.log
+/*Ejercicios Mensajes*/
+
+/*Document.write = el mensaje aparece dentro del documento, es decir en la pantalla de la web"*/
+document.write ("Esto es JavaScript");
+
+/*console.log = el mensaje aparece en la consola, esto puede manejar con funciones para ver el resultado*/
+console.log ("Esto es un mensaje en la cosnola");
+
+/*alert = el mensaje aparece como alerta en la parte superior de la pantalla*/
+alert ("Esto es una alerta");
+
+
+
